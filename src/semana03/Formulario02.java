b08ee8f841a328184b394a28fee4ea392f63a722
unknown <SUITE@SUITECE309-SERV.cibertec.edu.pe>
2024-04-10 21:06:04 -0500

    g

--- src/semana03/Formulario02.java
+++ src/semana03/Formulario02.java
@@ -6,10 +6,19 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+import javax.swing.JComboBox;
+import javax.swing.JButton;
+import javax.swing.JTextArea;
+import javax.swing.DefaultComboBoxModel;
 
 public class Formulario02 extends JFrame {
 
 	private JPanel contentPane;
+	private JTextField txtCantidad;
+	private JTextArea txtS;
+	private JComboBox cboCodigo;
 
 	/**
 	 * Launch the application.
@@ -35,8 +44,37 @@ public class Formulario02 extends JFrame {
 		setBounds(100, 100, 450, 300);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JLabel lblNewLabel = new JLabel("Cantidad");
+		lblNewLabel.setBounds(24, 22, 46, 14);
+		contentPane.add(lblNewLabel);
+		
+		txtCantidad = new JTextField();
+		txtCantidad.setBounds(115, 19, 175, 20);
+		contentPane.add(txtCantidad);
+		txtCantidad.setColumns(10);
+		
+		cboCodigo = new JComboBox();
+		cboCodigo.setModel(new DefaultComboBoxModel(new String[] {"1", "2", "3"}));
+		cboCodigo.setBounds(115, 58, 175, 20);
+		contentPane.add(cboCodigo);
+		
+		JButton btnProcesar = new JButton("Procesar");
+		btnProcesar.setBounds(313, 18, 89, 23);
+		contentPane.add(btnProcesar);
+		
+		JLabel lblCodigo = new JLabel("Codigo");
+		lblCodigo.setBounds(24, 61, 46, 14);
+		contentPane.add(lblCodigo);
+		
+		JButton btnLimpiar = new JButton("Limpiar");
+		btnLimpiar.setBounds(313, 57, 89, 23);
+		contentPane.add(btnLimpiar);
+		
+		txtS = new JTextArea();
+		txtS.setBounds(24, 109, 377, 124);
+		contentPane.add(txtS);
 	}
-
 }
