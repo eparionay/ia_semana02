8a05f9a8ff41987395d2504267bdb3a61761f41a
unknown <SUITE@SUITECE309-SERV.cibertec.edu.pe>
2024-04-10 20:23:54 -0500

    cambios

--- src/semana03/Formulario01.java
+++ src/semana03/Formulario01.java
@@ -12,13 +12,14 @@ import javax.swing.JButton;
 import javax.swing.JTextArea;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
+import javax.swing.JComboBox;
 
 public class Formulario01 extends JFrame {
 
 	private JPanel contentPane;
-	private JTextField txtCantidad;
-	private JTextField txtPrecio;
 	private JTextArea txtS;
+	private JTextField txtCantidad;
+	private JComboBox cboCodigo;
 
 	/**
 	 * Launch the application.
@@ -48,17 +49,12 @@ public class Formulario01 extends JFrame {
 		contentPane.setLayout(null);
 		
 		JLabel lblCantidad = new JLabel("Cantidad");
-		lblCantidad.setBounds(22, 30, 46, 14);
+		lblCantidad.setBounds(22, 67, 46, 14);
 		contentPane.add(lblCantidad);
 		
-		JLabel lblPrecio = new JLabel("Precio");
-		lblPrecio.setBounds(22, 67, 46, 14);
+		JLabel lblPrecio = new JLabel("Codigo");
+		lblPrecio.setBounds(22, 30, 46, 14);
 		contentPane.add(lblPrecio);
-		
-		txtCantidad = new JTextField();
-		txtCantidad.setBounds(78, 27, 191, 20);
-		contentPane.add(txtCantidad);
-		txtCantidad.setColumns(10);
 		
 		JButton btnProcesar = new JButton("Procesar");
 		btnProcesar.addActionListener(new ActionListener() {
@@ -89,6 +85,24 @@ public class Formulario01 extends JFrame {
 				}
 
 				txtS.append("Obsequio : " + regalo);
+				/*
+				xxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+				El nombre es : Juan
+				La edad es   : 20
+				El sueldo es : 2500
+				xxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+				*/
+				String nombre= "Juan";
+				int edad = 20;
+				double sueldo = 2500;
+				String resultado;
+				resultado = "El nombre es : " + nombre + "\n" +
+							"La edad es   : " + edad   + "\n" +
+							"El sueldo es : " + sueldo;
+
+				txtS.append(resultado);
+
+
 
 			
 			
@@ -101,13 +115,17 @@ public class Formulario01 extends JFrame {
 		btnLimpiar.setBounds(299, 63, 89, 23);
 		contentPane.add(btnLimpiar);
 		
-		txtPrecio = new JTextField();
-		txtPrecio.setBounds(78, 64, 191, 20);
-		contentPane.add(txtPrecio);
-		txtPrecio.setColumns(10);
-		
 		txtS = new JTextArea();
 		txtS.setBounds(22, 103, 370, 120);
 		contentPane.add(txtS);
+		
+		txtCantidad = new JTextField();
+		txtCantidad.setBounds(87, 64, 86, 20);
+		contentPane.add(txtCantidad);
+		txtCantidad.setColumns(10);
+		
+		cboCodigo = new JComboBox();
+		cboCodigo.setBounds(87, 27, 130, 20);
+		contentPane.add(cboCodigo);
 	}
 }
